44e4ae37bac1a4998eb28b91c4bda4d10b9976cc
Jerome-Ribaldone <jerome.ribaldone@my.jcu.edu.au>
2020-11-16 16:02:30 +1000

    prac 09

--- cleanup_files.py
+++ cleanup_files.py
@@ -21,28 +21,6 @@ def main():
             new_name = get_fixed_filename(filename)
             print(new_name)
 
-            # NOTE: These options won't just work...
-            # they show you ways of renaming and moving files,
-            # but you need to have valid filenames. You can't make a file with
-            # a blank name, and on Windows you can't have files with the same
-            # characters but different case (e.g. a.TXT and A.txt)
-            # So, you need to get valid filenames before you can use these.
-
-            # Option 1: rename file to new name - in place
-            # os.rename(filename, new_name)
-
-            # Option 2: move file to new place, with new name
-            # shutil.move(filename, 'temp/' + new_name)
-
-            # Processing subdirectories using os.walk()
-
-            # os.chdir('..')  # .. means "up" one directory
-            # for dir_name, subdir_list, file_list in os.walk('.'):
-            #     print("In", dir_name)
-            #     print("\tcontains subdirectories:", subdir_list)
-            #     print("\tand files:", file_list)
-
-
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
     # First, replace the spaces and .TXT (the easy part)
